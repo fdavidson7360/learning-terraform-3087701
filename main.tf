c5e6f2132defc15f67d97139c8ca26fd819a11e8
FranChell Davidson <39355483+fdavidson7360@users.noreply.github.com>
2024-08-07 14:24:09 -0700

    Trying to revert changes

--- main.tf
+++ main.tf
@@ -35,20 +35,17 @@ module "blog_vpc" {
   }
 }
 
-module "autoscaling" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-  version = "7.7.0"
-  
-  name = "blog"
-  min_size = 1
-  max_size = 2
+resource "aws_instance" "blog" {
+  ami           = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
 
-  vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns 
-  security_groups = [module.blog_sg.security_group_id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
-  image_id           = data.aws_ami.app_ami.id
-  instance_type      = var.instance_type
+  subnet_id = module.blog_vpc.public_subnets[0]
+
+  tags = {
+    Name = "Learning Terraform"
+  }
 }
 
 module "blog_alb" {
@@ -78,7 +75,7 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      #target_id        = aws_instance.blog.id
+      target_id        = "aws_instance.blog.id"
     }
   }
 
