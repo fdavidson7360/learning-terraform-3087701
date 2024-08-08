041b0391148a0e0619ebcd71187a94ff27c11845
FranChell Davidson <39355483+fdavidson7360@users.noreply.github.com>
2024-08-07 17:22:47 -0700

    target ID

--- main.tf
+++ main.tf
@@ -74,13 +74,10 @@ module "blog_alb" {
   }
 
   target_groups = {
-    ex-instance = {
-      name_prefix      = "blog-"
-      protocol         = "HTTP"
-      port             = 80
-      target_type      = "instance"
-      target_id        = aws_instance.blog.id
-    }
+    name_prefix      = "blog-"
+    protocol         = "HTTP"
+    port             = 80
+    target_type      = "instance"
   }
 
   tags = {
