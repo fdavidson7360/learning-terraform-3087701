5b33a495b120beb791c13ace8d2b1d01e8037cf4
FranChell Davidson <39355483+fdavidson7360@users.noreply.github.com>
2024-08-07 17:51:40 -0700

    Trying to replace depolyment code

--- main.tf
+++ main.tf
@@ -37,32 +37,30 @@ module "blog_vpc" {
 
 module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "7.7.0"
-  
+  version = "6.5.2"
+
   name = "blog"
-  min_size = 1
-  max_size = 2
 
+  min_size            = 1
+  max_size            = 2
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = ["module.blog_alb.target_group_arns"]
+  target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
-
-  image_id      = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
 
 module "blog_alb" {
-  source = "terraform-aws-modules/alb/aws"
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
 
-  name            = "blog-alb"
+  name = "blog-alb"
 
   load_balancer_type = "application"
 
-  vpc_id          = module.blog_vpc.vpc_id
-  subnets         = module.blog_vpc.public_subnets
-  security_groups = [module.blog_sg.security_group_id]
-
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
 
   target_groups = [
     {
@@ -83,7 +81,6 @@ module "blog_alb" {
 
   tags = {
     Environment = "dev"
-    Project     = "Example"
   }
 }
 
